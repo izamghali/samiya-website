495360cadb728836bc9d547b888c94ee28fe10d9
Izam Ghali <izamghali18@gmail.com>
2023-11-10 16:42:13 +0700

    CREATED: About component, but POSTPONED: the development of blurhash on Carousel

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function About(props) {
+    return (
+        <div className="cont bg-emerald-400">
+            
+        </div>
+    )
+};
+
+export default About;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { encode } from "blurhash";
 
 function Carousel(props) {
+
+    const testSrc = 'src/assets/carousel/1.webp';
+
+    // FIXME: Add blurhash while image loading
     return (
         <div>
-
+            <img src={testSrc} alt="" />
         </div>
     )
 };
