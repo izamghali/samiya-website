b248e556fe482330a3e4816ee8feb025394c3d31
Izam Ghali <izamghali18@gmail.com>
2023-11-12 23:29:09 +0700

    UPDATED: Service component btn background-color to be stone-50

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -22,9 +22,9 @@ function Service(props) {
     ]
 
     return (
-        <section className="cont bg-black">
+        <section className="cont black-cont">
             <h2 className="text-center text-white">Our Photography Services</h2>
-            <p className="text-center sub-heading text-darkGray">Enjoy professional photography services with Samiya Studio</p>
+            <p className="text-center font-mont margin-heading text-darkGray">Enjoy professional photography services with Samiya Studio</p>
 
             {/* Card Container */}
             <div className="card-container mt-8 flex-wrap">
@@ -43,9 +43,15 @@ function Service(props) {
                                 <h3 className="text-2xl text-white">{data.title} Photography</h3>
                                 <p className="text-darkGray">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             </div>
-                            <a href="https://wa.me/6289616041234" target="_blank" className="btn flex items-center gap-1 py-4 w-full justify-center font-bold text-2xl hover:bg-accentLess duration-300 hover:-translate-y-2">
+                            <a 
+                                href="https://wa.me/6289616041234" target="_blank" 
+                                className="
+                                flex items-center rounded-lg border-2 border-stone-50
+                                text-stone-950 bg-stone-50 gap-1 py-4 w-full justify-center font-bold 
+                                text-2xl hover:bg-stone-950 duration-300 hover:text-stone-50
+                                hover:-translate-y-2 group">
                                 Book Now
-                                <svg class="bi bi-arrow-up-right w-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                                <svg class="bi bi-arrow-up-right w-6 fill-stone-950 group-hover:fill-stone-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
                                 </svg>
                             </a>
