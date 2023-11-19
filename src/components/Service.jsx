0e35d71d1c3c201ace7c7c49d9e87cfa7c5e55be
Izam Ghali <izamghali18@gmail.com>
2023-11-20 00:51:45 +0700

    UPDATED: the image size on sm screen on Service component. Used items-center to make the image on the center

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -36,20 +36,26 @@ function Service(props) {
                 {serviceData.map(data => {
                     return <div className="
                                 border-2 border-darkGray rounded-lg 
-                                max-w-sm lg:max-w-sm sm:max-w-full 
+                                lg:w-[24rem] sm:max-w-full 
+                                sm:h-fit lg:h-full
                                 flex flex-col lg:flex-col sm:flex-row
                                 gap-2 sm:gap-4 lg:gap-2 p-4 
                                 snap-start lg:snap-align-none
                                 ">
-                                <div className="relative h-[20rem] w-full">
-                                    <img className="rounded-lg w-full h-full object-center object-cover" src={data.img} alt="" />
+                                <div className="relative flex items-center 
+                                        lg:h-[20rem] lg:w-full 
+                                        sm:h-[15rem] sm:w-[45rem] 
+                                        h-[20rem] w-full
+                                        rounded-lg overflow-hidden
+                                    ">
+                                    <img className="" src={data.img} alt="" />
                                     <span className="absolute top-4 right-4 bg-black opacity-80 text-white p-1 rounded-md">
                                         Photo
                                     </span>
                                 </div>
 
                                 {/* content */}
-                                <div className="flex flex-col justify-evenly">
+                                <div className="flex flex-col sm:justify-between lg:justify-evenly justify-evenly w-auto">
                                     <div className="lg:mb-8 sm:mb-0 mb-8">
                                         <h3 className="text-2xl text-white">{data.title} Photography</h3>
                                         <p className="text-darkGray lg:min-h-[8rem]">{data.desc}</p>
@@ -57,13 +63,14 @@ function Service(props) {
                                     <a 
                                         href="https://wa.me/6289616041234" target="_blank" 
                                         className="
-                                        flex items-center justify-center gap-1 py-4
+                                        flex items-center justify-center gap-1 
+                                        md:py-4 sm:py-2 py-4
                                         bg-stone-50 border-2 border-stone-50 rounded-lg
-                                        w-full text-2xl font-bold text-stone-950
+                                        w-full md:text-2xl sm:text-xl text-2xl md:font-bold sm:font-semibold font-bold text-stone-950
                                         lg:hover:bg-stone-950 lg:hover:text-stone-50
                                         lg:hover:-translate-y-2 group duration-300">
                                         Book Now
-                                        <svg class="bi bi-arrow-up-right w-6 fill-stone-950 lg:group-hover:fill-stone-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                                        <svg class="bi bi-arrow-up-right lg:w-6 md:w-5 sm:w-5 w-6 fill-stone-950 lg:group-hover:fill-stone-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
                                         </svg>
                                     </a>
