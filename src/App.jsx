8cb375f3c76bcf2f737c32b49f38afffe62ed8a5
Izam Ghali <izamghali18@gmail.com>
2023-11-26 19:17:21 +0700

    UPDATED: the empty div on App component, to adjust snap scroll on mobile device

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import Gallery from './components/Gallery'
 import HomePage from './components/HomePage'
 import ContactPage from './components/ContactPage'
 
+// FIXME: key for each component we're mapping
+// FIXME: check react dom client error on the console
+
 function App() {
 
   const [ page, setPage ] = useState(false)
+  const [ category, setCategory ] = useState('')
 
   function snapScroll() {
     const the = document.getElementById('the-html');
@@ -26,10 +30,13 @@ function App() {
     <div className="dark">
       <ContactPage/>
 
-      { !page ? <div className='bg-stone-950 h-10'></div> : <div></div> } {/* NOTE:  ignore this; this exists so the scroll snaps back to Navbar */}
-      <Navbar page={page} setPage={setPage}/>
+      { !page ? <div className='bg-stone-950 h-10 hidden sm:block'></div> : <div></div> } {/* NOTE:  ignore this; this exists so the scroll snaps back to Navbar */}
+      <Navbar 
+        page={page} setPage={setPage}
+        setCategory={setCategory} 
+      />
 
-      { page ? <Gallery/> : <HomePage/> }
+      { page ? <Gallery /> : <HomePage/> }
       
     </div>
     </>
