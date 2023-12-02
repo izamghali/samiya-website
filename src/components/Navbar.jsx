1b1cd026e0dba751d3e4cc3fc2a1d55583a20b6d
Izam Ghali <izamghali18@gmail.com>
2023-12-02 20:18:50 +0700

    UPDATED: typo svg attribute like fillRule, referrerPolicy, clipRule

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -105,7 +105,7 @@ function Navbar(props) {
                     <button className="relative flex items-center gap-1" onClick={handleDropDown} id="drop-down">
                         Gallery
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                            <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                         </svg>
 
                         {/* {<Transition
