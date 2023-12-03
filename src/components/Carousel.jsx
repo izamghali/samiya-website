b8d62f21669b9d597506c7ebb6093f4b899774d3
Izam Ghali <izamghali18@gmail.com>
2023-12-03 16:18:35 +0700

    UPDATED: Carousel to have Blurhash while loading the image using useEffect and Blurhash comp

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react"
 import carousel1 from '../assets/carousel/1.webp';
+import { Blurhash } from "react-blurhash";
 
 function Carousel(props) {
 
+    const [ imageLoaded, setImageLoaded ] = useState(false)
+
+    useEffect(() => {
+        const img = new Image()
+        img.onload = () => {
+            setImageLoaded(true)
+        }
+        img.src = carousel1
+    }, [carousel1])
+
     // FIXME: Add blurhash while image loading
     return (
-        <section className="">
-            <img src={carousel1} alt="" />
+        <section className="overflow-hidden">
+
+            {/* NOTE: Blurhash */}
+            { imageLoaded ? 
+                <img src={carousel1} alt="" /> : 
+                <Blurhash
+                    className="scale-105"
+                    hash="LEAvd%0g58}?E3xDt6R.0N-oxsIq"
+                    width={window.innerWidth}
+                    height={window.innerWidth * 0.55}
+                    resolutionX={128}
+                    resolutionY={128}
+                    punch={1}
+                />
+            }
         </section>
     )
 };
